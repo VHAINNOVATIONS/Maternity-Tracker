e43a51d1a7a091a6c96041d8ca1cd8cdb0097a2d
RandyDSS <rpoynter@dssinc.com>
2016-09-16 10:12:43 -0700

    Patient Portal 1.0.1

---
+++ Portal/va.gov.artemis.portal/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace va.gov.artemis.portal
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
